95b908226f31e6d73d7b6286fef27af55cbf9ded
EdZeno <eduard.kulcsickij.gut@gmail.com>
2020-01-26 10:57:02 +0000

    it takes a string with 2 characters

--- lib/duplicate.py
+++ lib/duplicate.py
@@ -1,2 +1,6 @@
 def duplicate(string):
+  a = list(string)
+  if len(a) > 1 and a[0] == a[1]:
+    return ')'
+  else:
     return '('
--- tests/test_duplicate.py
+++ tests/test_duplicate.py
@@ -3,3 +3,7 @@ from lib.duplicate import duplicate
 def test_duplicate_returns():
     string = 'i'
     assert duplicate(string) == '('
+
+def test_duplicate_two_characters():
+    string = 'ii'
+    assert duplicate(string) == ')'
