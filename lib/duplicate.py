19dd832efa9102585901638d8597040d9264819d
EdZeno <eduard.kulcsickij.gut@gmail.com>
2020-01-27 16:27:25 +0000

    passed the test

--- lib/duplicate.py
+++ lib/duplicate.py
@@ -1,12 +1,15 @@
 def duplicate(string):
   a = list(string)
-  string1 = ""
+  string1 = []
 
   for c in a:
+
     print(a)
     print(c)
     print(string1)
-    if len(a) > 1 and a[0] == a[1]:
-      string1 + '))'
+    if a.count(c) > 1:
+      string1.append(')')
     else:
-      string1 + '('
+      string1.append('(')
+
+  return ''.join(string1)
